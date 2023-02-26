1de09e2aab2b7a7d71150f64c2ea2b984971bca0
Rick Silliker <ricksilliker@gmail.com>
2023-02-25 19:42:56 -0600

    start test level

--- Assets/Code/Ingredient.cs
+++ Assets/Code/Ingredient.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Code
 {
     public class Ingredient : MonoBehaviour
     {
+        [Serializable]
+        public enum IngredientState
+        {
+            Raw,
+            UnderCooked,
+            Cooked,
+            OverCooked,
+            Frozen
+        }
+        
         [SerializeField] private IngredientData data;
+        [SerializeField] private IngredientState state;
     }
 }
--- Assets/Code/IngredientData.cs
+++ Assets/Code/IngredientData.cs
@@ -5,11 +5,11 @@ namespace Code
     [CreateAssetMenu(fileName = "IngredientData", menuName = "Cooking", order = 0)]
     public class IngredientData : ScriptableObject
     {
-        [SerializeField] private string name;
-        [SerializeField] private GameObject model;
+        [SerializeField] private string displayName;
         [SerializeField] private float power;
         [SerializeField] private float defense;
         [SerializeField] private float health;
         [SerializeField] private float quickness;
+        [SerializeField] private float sight;
     }
 }
